95e94e72b377136a73fe58732d92e69243d1df25
Kévin DANG <kevindang@hotmail.fr>
2020-08-28 01:24:31 +0200

    add totality of the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import{Observable} from "rxjs/Observable";
+import  'rxjs/Rx';
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+  secondes: number;
+  counterSusbscription: Subscription;
+
+  constructor() {
+  }
+
+  ngOnInit(){
+    const counter = Observable.interval(1000);
+    this.counterSusbscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.counterSusbscription.unsubscribe()
+  }
+
+}
